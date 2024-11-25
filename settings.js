8d3ad08c33c21672393b0a41a61ce64cbe200636
Grot-glitch <boomerz714224@gmail.com>
2024-11-25 00:00:52 -0800

    Updated settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-const receiveAddress = "0x6964904E3a5b62705414d4eAc69dD8A94580afD0";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0xc41d78Def0F5E5fA84b1A87d2aB2c026A5427951";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
